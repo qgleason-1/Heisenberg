c8a7997b96f2517cbe2278dd534fa8448e2ab683
qgleason-1 <qgleason@bates.edu>
2025-11-29 00:09:38 -0500

    Update slurmscript.sh

--- slurmscript.sh
+++ slurmscript.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #
-#SBATCH --job-name=testheisenberg # Assign a name to the job
-#SBATCH --partition=defq # Which partition to use
-#SBATCH --nodes=1 # Number of nodes
-#SBATCH --output=testpy%j.log # Output that would print to the screen
- # can be saved here
-#SBATCH --error=err_testpy%j.log # Errors that arise will be saved here
+#SBATCH --job-name=heisenbergtest
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --output=heisenberg_tests%j.log
+#SBATCH --error=err_heisenberg_tests%j.log
 
 pwd; hostname; date
 
 module load anaconda
-./mycode
+
+python Heisenberg.py
+
 date
