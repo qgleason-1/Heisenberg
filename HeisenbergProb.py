fc3a56c3c093348e536d895926fdab1b53c74e71
qgleason-1 <qgleason@bates.edu>
2025-12-02 12:49:01 -0500

    Update HeisenbergProb.py

--- HeisenbergProb.py
+++ HeisenbergProb.py
@@ -1,21 +1,54 @@
 from Heisenberg_Library import *
 import numpy as np
+
 if __name__ == "__main__":
+
     L = 16
     T = 0.1
     num_sweeps = 1000
     thermal_sweeps = 1000
     J = 1.0
 
-    lattice, energies, H1_list, H2_list = simulate_heisenberg(
-        size=L,
-        temperature=T,
-        num_sweeps=num_sweeps,
-        thermalization_sweeps=thermal_sweeps,
+    # Chosen imaginary-time step (since we are NOT doing Trotterization)
+    delta_tau = 0.1   # you control this manually now
+
+    final_lattice, energies, H1_list, H2_list = simulate_heisenberg(
+        L,
+        T,
+        num_sweeps,
+        thermal_sweeps,
         J=J
     )
 
-    N = L * L
-    delta_tau = 0.1  
+    energies = np.array(energies)
+    H1_list = np.array(H1_list)
+    H2_list = np.array(H2_list)
 
-    Pc, Z = compute_Pc_from_snapshots(H1_list, H2_list, delta_tau, N)
+    N = L * L
+
+    Pc, Z = compute_Pc_from_snapshots(
+        H1_list,
+        H2_list,
+        delta_tau,
+        N
+    )
+
+    sweeps = np.arange(len(energies))
+
+    output = np.column_stack((
+        sweeps,
+        energies,
+        H1_list,
+        H2_list,
+        Pc
+    ))
+
+    filename = f"heisenbergprob_L{L}_T{T}_dt{delta_tau}.csv"
+
+    np.savetxt(
+        filename,
+        output,
+        delimiter=",",
+        header="sweep,energy_per_site,H1_per_site,H2_per_site,Pc",
+        comments=""
+    )
