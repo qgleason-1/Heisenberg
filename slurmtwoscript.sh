960a16a1cd0e8e379f089e02bb164ee5722295c1
qgleason-1 <qgleason@bates.edu>
2025-12-02 12:43:35 -0500

    Update slurmtwoscript.sh

--- slurmtwoscript.sh
+++ slurmtwoscript.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --job-name=heisenbergtest
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --output=heisenberg_tests%j.log
+#SBATCH --error=err_heisenberg_tests%j.log
 
+pwd; hostname; date
+
+module load anaconda
+
+python HeisenbergAlt.py
+
+date
